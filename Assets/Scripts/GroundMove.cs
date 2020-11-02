be1f6781d255cf04f9344299edc66dde8b9ef27f
Hayden Rose <50933095+rosehay22@users.noreply.github.com>
2020-11-01 18:30:38 -0600

    test

---
+++ Assets/Scripts/GroundMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; public class GroundMove : MonoBehaviour
+{
+    int speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+      
+        if (Input.GetKey(KeyCode.W))
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A) && transform.position.x < 140 )
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x > -140)
+            transform.Translate(-Vector3.up * speed * Time.deltaTime);
+    }
+}
